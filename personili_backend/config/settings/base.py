0c15cdd77f005302ce87410d4d6accde6841ec70
heytem <heytem.boumaza@gmail.com>
2024-02-18 07:07:11 -1200

    change rest response type

--- personili_backend/config/settings/base.py
+++ personili_backend/config/settings/base.py
@@ -298,6 +298,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
