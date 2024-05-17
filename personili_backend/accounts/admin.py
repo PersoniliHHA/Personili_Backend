c72f7ff69a8d359fb321301dae8e3c20458840c7
heytem <heytem.boumaza@gmail.com>
2024-05-17 09:16:41 -1200

    add role permission model

--- personili_backend/accounts/admin.py
+++ personili_backend/accounts/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from accounts.models import Account, AccountProfile, DeliveryAddress, PaymentMethod, Feedback, AccountBlacklist, Wallet, Transaction
+from accounts.models import Account, AccountProfile, DeliveryAddress, PaymentMethod, Feedback, AccountBlacklist, Wallet, Transaction, RolePermission, Role, Permission
 
 # Add all usermanagement app models to admin site
 admin.site.register(Account)
 admin.site.register(AccountProfile)
+admin.site.register(RolePermission)
+admin.site.register(Role)
+admin.site.register(Permission)
 admin.site.register(DeliveryAddress)
 admin.site.register(PaymentMethod)
 admin.site.register(Feedback)
