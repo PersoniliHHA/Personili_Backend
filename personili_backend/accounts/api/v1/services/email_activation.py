44d183708acac5a9a8e04884cd1921648b06efdf
heytem <heytem.boumaza@gmail.com>
2024-05-29 23:25:01 -1200

    add print

--- personili_backend/accounts/api/v1/services/email_activation.py
+++ personili_backend/accounts/api/v1/services/email_activation.py
@@ -54,6 +54,8 @@ def send_email_activation_link(email_to_activate: str,
         "activation_link": activation_link
     }
 
+    print("activation link: ", activation_link)
+
     # Send the email
     brevo_engine.send_email(
         to_email=email_to_activate,
