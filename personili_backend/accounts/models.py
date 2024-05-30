22a2cab599a799301a4c095c4590742248ed7a3a
heytem <heytem.boumaza@gmail.com>
2024-05-29 23:26:56 -1200

    add more debug prints

--- personili_backend/accounts/models.py
+++ personili_backend/accounts/models.py
@@ -179,10 +179,13 @@ class ActionToken(TimeStampedModel):
         try:
             token = cls.objects.get(token=token, token_type=type)
             if token.expiry_date > datetime.now():
+                print("token is valid")
                 token.delete()
                 return True, token.account.id
+            print("token expired")
             return False, None
         except cls.DoesNotExist:
+            print("token does not exist")
             return False, None
 
     def __str__(self) -> str:
