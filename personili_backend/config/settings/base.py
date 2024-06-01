273f9df4d41cc11d463edcfa0f2e197006ce3c75
heytem <heytem.boumaza@gmail.com>
2024-06-01 05:29:17 -1200

    update env files

--- personili_backend/config/settings/base.py
+++ personili_backend/config/settings/base.py
@@ -57,8 +57,8 @@ DATE_FORMAT = "d-m-Y"
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-#DATABASES = {"default": env.db("DATABASE_URL")}
-#DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
