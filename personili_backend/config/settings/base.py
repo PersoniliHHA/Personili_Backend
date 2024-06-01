ac38d19966ef74e8f65056564e20c82633247de8
heytem <heytem.boumaza@gmail.com>
2024-06-01 08:34:45 -1200

    change base settting

--- personili_backend/config/settings/base.py
+++ personili_backend/config/settings/base.py
@@ -57,7 +57,7 @@ DATE_FORMAT = "d-m-Y"
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES = {"default": env.db("DATABASE_URL", default="sqlite:///db.sqlite3")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
