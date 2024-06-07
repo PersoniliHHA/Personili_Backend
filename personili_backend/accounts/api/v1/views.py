8b645ad72f0dec768ac90577e6941cf0489d2aaf
heytem <heytem.boumaza@gmail.com>
2024-06-07 09:25:50 -1200

    update log debug messages

--- personili_backend/accounts/api/v1/views.py
+++ personili_backend/accounts/api/v1/views.py
@@ -371,7 +371,8 @@ class AccountProfileViewSet(viewsets.ModelViewSet):
         # Check that the path parameters are the same as the authenticated user
         if request.user.id != account_id or request.user.profile.id != profile_id:
             print("condition 2")
-            print(request.user.id, account_id, request.user.profile.id, profile_id)
+            print(request.user.id != account_id)
+            print(request.user.profile.id != profile_id)
             print("condition 2")
             return Response({"error": "BAD_REQUEST"}, status=status.HTTP_400_BAD_REQUEST)
         
