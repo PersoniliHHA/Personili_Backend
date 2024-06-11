7fc528478a05dd5bade38625135da2245a3705fc
heytem <heytem.boumaza@gmail.com>
2024-06-11 04:35:05 -1200

    update the facotyr with json values

--- personili_backend/db_scripts/factory_generator.py
+++ personili_backend/db_scripts/factory_generator.py
@@ -1,8 +1,13 @@
 from accounts.factories import AccountFactory, AccountProfileFactory
 
 
+def personili_local_db_data(nb_accounts: int=20):
+
+    # First create the accounts
+    for _ in range(nb_accounts):
+        account = AccountFactory()
+        account_profile = AccountProfileFactory(account=account)
 
 
-for _ in range(10):
-    account = AccountFactory()
-    account_profile = AccountProfileFactory(account=account)
+if __name__ == '__main__':
+    personili_local_db_data()
