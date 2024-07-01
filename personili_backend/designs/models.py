a1e8b0a29c7f5e2c32619bdc2ca47287a6371cf8
heytem <heytem.boumaza@gmail.com>
2024-07-01 22:31:41 +0200

    update and fix the design get api

--- personili_backend/designs/models.py
+++ personili_backend/designs/models.py
@@ -424,7 +424,7 @@ class Design(TimeStampedModel):
                            .annotate(num_likes=models.Count('design_likes')) 
                            .select_related('store__storeprofile', 'workshop__organization__orgprofile', 'theme')
                            .prefetch_related('design_previews')
-                           .order_by('-num_likes')[offset:limit])
+                           .order_by('-num_likes'))[offset:limit]
 
         result = {"designs_list":[]}
         for design in designs:
