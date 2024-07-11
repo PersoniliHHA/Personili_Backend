2a4223c5cc52d5187ec2da95e8624fd27a35d796
heytem <heytem.boumaza@gmail.com>
2024-07-11 22:53:17 +0200

    update design catalog api

--- personili_backend/personalizables/urls.py
+++ personili_backend/personalizables/urls.py
@@ -4,11 +4,11 @@ from rest_framework.routers import DefaultRouter
 # django imports
 from django.urls import path, include
 
-from personalizables.api.v1.views import CategoryViewSet, PersonalizationTypeViewSet
+from personalizables.api.v1.views import CategoryViewSet, DepartmentViewSet, PersonalizationTypeViewSet
 
 router = DefaultRouter()
 router.register('v1/personalizables', CategoryViewSet, basename='categories')
-router.register('v1/personalizables', PersonalizationTypeViewSet, basename='departments')
+router.register('v1/personalizables', DepartmentViewSet, basename='departments')
 router.register('v1/personalizables', PersonalizationTypeViewSet, basename='personalization-types')
 urlpatterns = [
  path('', include(router.urls)),
