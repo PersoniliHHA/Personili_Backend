66ec114b5a746fd9007f787e8aa0b09c9aaf4b99
heytem <heytem.boumaza@gmail.com>
2024-07-23 23:52:36 +0200

    update design model and some shit

--- personili_backend/designs/api/v1/views.py
+++ personili_backend/designs/api/v1/views.py
@@ -398,6 +398,10 @@ class DesignsViewSet(viewsets.ViewSet):
         mode = request.data.get('mode', "text-to-image")
         sd3_model = request.data.get('sd3_model', "SD 1.6")
         style_preset = request.data.get('style_preset', "3d-model")
+
+        # check that parameters are not empty
+        if not stability_model or not aspect_ratio or not output_format or not mode or not sd3_model or not style_preset:
+            return Response({"error": "BAD_REQUEST"}, status=400)
         
         try:
             # Generate the image
